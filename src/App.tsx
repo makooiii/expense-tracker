a2645a4647f0a8fdd081d13e7c9d2f89e3e52db4
Mark Sapiandante <msapiandante.ccheetah@gmail.com>
2024-03-22 12:11:02 +0800

    added expense list

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import { useState } from "react";
 import "./App.css";
+import ExpenseList from "./components/expense-tracker/components/ExpenseList";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const expenses = [
+    { id: 1, description: "Yeees", amount: 50, category: "Utilities" },
+    { id: 2, description: "nooo", amount: 50, category: "Utilities" },
+    { id: 3, description: "maybee", amount: 50, category: "Utilities" },
+    { id: 4, description: "noplease", amount: 50, category: "Utilities" }
+  ];
 
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <ExpenseList expenses={expenses} onDelete={() => console.log("deleted")} />
+    </div>
+  );
 }
 
 export default App;
