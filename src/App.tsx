1af784140a2373973caa9b656d31e24bc699650a
Mark Sapiandante <msapiandante.ccheetah@gmail.com>
2024-03-22 12:16:42 +0800

    added list delete function

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,16 @@ import "./App.css";
 import ExpenseList from "./components/expense-tracker/components/ExpenseList";
 
 function App() {
-  const expenses = [
+  const [expenses, setExpenses] = useState([
     { id: 1, description: "Yeees", amount: 50, category: "Utilities" },
     { id: 2, description: "nooo", amount: 50, category: "Utilities" },
     { id: 3, description: "maybee", amount: 50, category: "Utilities" },
     { id: 4, description: "noplease", amount: 50, category: "Utilities" }
-  ];
+  ]);
 
   return (
     <div className="App">
-      <ExpenseList expenses={expenses} onDelete={() => console.log("deleted")} />
+      <ExpenseList expenses={expenses} onDelete={id => setExpenses(expenses.filter(e => e.id !== id))} />
     </div>
   );
 }
